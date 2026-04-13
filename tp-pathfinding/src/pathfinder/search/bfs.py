b4650807ab8684b2cf58e565759ee84a2bc45e5c
sebaschiaou <sebastian@buildintelligencesolutions.com>
2026-04-14 00:54:58 +0200

    Agrego el bfs, espectacular esto

--- tp-pathfinding/src/pathfinder/search/bfs.py
+++ tp-pathfinding/src/pathfinder/search/bfs.py
@@ -25,5 +25,28 @@ class BreadthFirstSearch:
         # Initialize frontier with the root node
         # TODO Complete the rest!!
         # ...
+        # 3 if (problema.test-objetivo(n₀.estado)) then return solución(n₀)
+        if grid.objective_test(root.state):
+            return Solution(root,reached)
+        # frontera ← Cola()
+        frontier = QueueFrontier()
+        # frontera.encolar(n₀)
+        frontier.add(root)
 
-        return NoSolution(reached)
+        while True:
+        # if frontera.vacía() then return fallo
+            if frontier.is_empty():
+                return NoSolution(reached)
+            # 9 n ← frontera.desencolar()
+            node = frontier.remove()
+            # forall a in problema.acciones(n.estado) do
+            for action in grid.actions(node.state):
+                #s’ ← problema.resultado(n.estado, a)
+                new_state = grid.result(node.state, action)
+                if new_state not in reached.keys():
+                    new_node = Node("", state=new_state, cost = node.cost+grid.individual_cost(node.state,action), parent=node, action=action)
+                    if grid.objective_test(new_state):
+                        return Solution(new_node,reached)                    
+                    reached[new_state] = True
+                    frontier.add(new_node)
+        
